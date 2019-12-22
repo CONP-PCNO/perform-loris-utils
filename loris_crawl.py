5a178f7850bec35c34a78afa2986f1aa3207308d
mathdugre <math.dugre@gmail.com>
2019-12-22 15:51:04 -0500

    Black formatting

--- loris_crawl.py
+++ loris_crawl.py
@@ -11,25 +11,30 @@ FILE_EXCLUDE = [".DS_Store", ".gitattributes"]
 
 
 def _get_args():
-    parser = argparse.ArgumentParser(description="Datalad crawler for LORIS file uploader")
-    parser.add_argument(
-        "loris_url", type=str, help="URL of a LORIS Candidate API"
+    parser = argparse.ArgumentParser(
+        description="Datalad crawler for LORIS file uploader"
     )
+    parser.add_argument("loris_url", type=str, help="URL of a LORIS Candidate API")
     parser.add_argument(
-        "candidates", type=str, help="Path to file holding a list of candidates ID to upload on Zenodo."
+        "candidates",
+        type=str,
+        help="Path to file holding a list of candidates ID to upload on Zenodo.",
     )
     return parser.parse_args()
 
 
 def candidate_download(candidates_file):
-    
+
     # Verify if folder already exists
     if os.path.exists("candidates"):
         answer = ""
         while answer.lower() not in ["y", "n"]:
-            answer = input("A folder \"candidates\" folder already exist. Are you sure you want to continue [Y/n]? ")
+            answer = input(
+                'A folder "candidates" folder already exist. Are you sure you want to continue [Y/n]? '
+            )
 
-        if answer.lower() != "y": system.exit(0)
+        if answer.lower() != "y":
+            system.exit(0)
     else:
         os.mkdir("candidates")
 
@@ -43,9 +48,14 @@ def candidate_download(candidates_file):
         if not os.path.exists(id_):
             datalad.api.create(id_)
         os.chdir(id_)
-        datalad.api.crawl_init(save=True, template="loris-candidate-api", args={"url": "{}/{}".format(args.loris_url, id_)})
+        datalad.api.crawl_init(
+            save=True,
+            template="loris-candidate-api",
+            args={"url": "{}/{}".format(args.loris_url, id_)},
+        )
         datalad.api.crawl()
-        os.chdir(pwd)       
+        os.chdir(pwd)
+
 
 if __name__ == "__main__":
     args = _get_args()
