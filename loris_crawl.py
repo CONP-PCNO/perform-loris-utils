4e93924b91af136e15d619f7adb3c1dadce7a27e
mathdugre <math.dugre@gmail.com>
2019-12-22 15:49:41 -0500

    [Added] loris_url argument and [Removed] f-string for compatibility

--- loris_crawl.py
+++ loris_crawl.py
@@ -13,6 +13,9 @@ FILE_EXCLUDE = [".DS_Store", ".gitattributes"]
 def _get_args():
     parser = argparse.ArgumentParser(description="Datalad crawler for LORIS file uploader")
     parser.add_argument(
+        "loris_url", type=str, help="URL of a LORIS Candidate API"
+    )
+    parser.add_argument(
         "candidates", type=str, help="Path to file holding a list of candidates ID to upload on Zenodo."
     )
     return parser.parse_args()
@@ -40,7 +43,7 @@ def candidate_download(candidates_file):
         if not os.path.exists(id_):
             datalad.api.create(id_)
         os.chdir(id_)
-        datalad.api.crawl_init(save=True, template="loris-candidate-api", args={"url": f"https://loris.concordia.ca/api/v0.0.2/candidates/{id_}"})
+        datalad.api.crawl_init(save=True, template="loris-candidate-api", args={"url": "{}/{}".format(args.loris_url, id_)})
         datalad.api.crawl()
         os.chdir(pwd)       
 
